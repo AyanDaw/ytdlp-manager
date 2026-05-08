94d121459abcc03ab0d86a901341d78f9c9a9219
AyanDaw <official.ayan.daw@gmail.com>
2026-05-08 16:24:10 +0530

    file_opener.py fixed
    changed the complex logic into basic simple logic

--- ytdlp-manager/modules/file_opener.py
+++ ytdlp-manager/modules/file_opener.py
@@ -9,17 +9,23 @@ def open_file(filepath: str) -> bool:
     if not os.path.exists(filepath):
         return False
     
-    open_ask = input("Open downloaded file? [Y/n]:> ").strip().lower()
-    if open_ask not in ('y', 'yes', ''):
-        return True
+    open_ask = input("Do you want to open the file? [Y/n]:> ").strip().lower()
+    if open_ask in ('y', 'yes', ''):
+        try:
+            if platform.system() == 'Windows':
+                subprocess.Popen(['start', '', filepath], shell=True)
+            elif platform.system() == 'Darwin':  # Mac
+                subprocess.Popen(['open', filepath])
+            else:  # Linux
+                subprocess.Popen(['xdg-open', filepath])
+            return True
+        except Exception as e:
+            return False
     
-    try:
-        if platform.system() == 'Windows':
-            subprocess.Popen(['start', '', filepath], shell=True)
-        elif platform.system() == 'Darwin':  # Mac
-            subprocess.Popen(['open', filepath])
-        else:  # Linux
-            subprocess.Popen(['xdg-open', filepath])
-        return True
-    except Exception as e:
-        return False
+    return False
+    
+
+if __name__ == "__main__":
+    filepath = input("Enter Your filepath:>")
+    filepath = filepath.strip().strip('"').strip("'")
+    open_file(filepath=filepath)
